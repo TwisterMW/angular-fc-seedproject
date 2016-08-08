ff71660c0413fbf5d9aa8411ff7ef8889f5b5402
TwisterMW <marc.hernandez@twiscript.com>
2016-08-08 05:40:05 +0200

    Environment fixes and improvements

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,46 +1,13 @@
 module.exports = function(grunt){
-  	// Project configuration.
+
 	grunt.initConfig({
-  		clean: ["dist", "docs"],
-
-  		cssmin: {
-			options: {
-				shorthandCompacting: false,
-				roundingPrecision: -1
-			},
-			target: {
-				files: {
-				  'dist/css/styles.min.css': ['bower_components/bootstrap/dist/css/bootstrap.min.css', 'assets/css/override.css']
-				}
-			}
-		},
-
-		open: {
-		    dev: {
-			    path: 'http://localhost:9000/dist/index.html',
-		      	app: 'chrome'
-		    },
-            docs: {
-                path: 'http://localhost:9000/docs/index.html',
-                app: 'chrome'
-            }
-		},
-
-        cachebreaker: {
-            dev: {
-                options: {
-                    match: ['scripts.min.js', 'styles.min.css'],
-                    replacement: 'md5',
-                    src: {
-                        path: 'dist/js/scripts.min.js'
-                    }
-                },
-                files: {
-                    src: ['dist/index.html']
-                }
-            }
+        // Remove content from directory list
+  		clean: {
+            all: ['dist', 'docs', '.tmp'],
+            temp: ['.tmp']
         },
 
+        // Copies files to output folder
         copy: {
             main: {
                 files: [
@@ -50,42 +17,136 @@ module.exports = function(grunt){
             }
         },
 
+        // Generates the documentation related to the application
         ngdocs: {
           all: ['app/**/*.js']
         },
 
-        ngmin: {
-            dist: {
-	            src: [
-  					'bower_components/jquery/dist/jquery.min.js',
-					'bower_components/bootstrap/dist/js/bootstrap.min.js',
-					'bower_components/angular/angular.min.js',
-					'bower_components/angular-route/angular-route.min.js',
-					'app/app.routes.js',
-					'app/controllers/mainCtrl.js'
-  				],
-	            dest: 'dist/js/scripts.min.js'
-           	}
+        // Automatically includes bower dependencies on index.html
+        wiredep: {
+            task: {
+                src: 'index.html'
+            },
         },
 
-	    serve: {
-	        options: {
-	            port: 9000
-	        }
-    	}
+        // Automatically detects bower_component updates and executes wiredep task
+        watch: {
+            js: {
+                files: ['app/*', 'app/**/*'],
+                tasks: ['jshint']
+            }
+        },
+
+        // Automatically detects JS errors
+        jshint: {
+            all: ['Gruntfile.js', 'app/**/*.js', 'app/*.js']
+        },
+
+
+        // Minifies all the JS dependencies of wiredep
+        uglify: {
+            options: {
+                compress: true
+            },
+            dev: { 
+                files: { 
+                    'dist/js/app.js': [
+                        require('wiredep')().js, 'app/*.js', 'app/**/*.js'
+                    ]
+                }
+            }
+        },
+
+        // Minifies all the CSS dependencies of wiredep
+        cssmin: {
+            minify: {
+                files: { 
+                    'dist/css/app.css': [
+                        require('wiredep')().css, 'assets/css/*.css'
+                    ]
+                }
+           }
+        },
+
+        // Mocks a server on custom port
+        connect: {
+            dev: {
+                options: {
+                    port: 9000,
+                    base: '.',
+                    open: true
+                }
+            },
+            dist: {
+                options: {
+                    port: 9001,
+                    base: 'dist/',
+                    open: true,
+                    keepalive: true
+                }
+            },
+            docs:{
+                options: {
+                    port: 9002,
+                    base: 'docs/',
+                    open: true,
+                    keepalive: true
+                }
+            }
+        },
+
+        // Target for configure minifying tasks
+        useminPrepare: {
+            html: 'index.html',
+            options: {
+                src: '.',
+                dest: 'dist/'
+            }
+        },
+
+        // Target for replacing dependencies
+        usemin: {
+            html: 'dist/index.html'
+        }
     });
 
-    grunt.loadNpmTasks('grunt-open');
-    grunt.loadNpmTasks('grunt-ngmin');
-    grunt.loadNpmTasks('grunt-serve');
+    // Tasks loading
+    grunt.loadNpmTasks('grunt-usemin');
     grunt.loadNpmTasks('grunt-ngdocs');
-    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-wiredep');
+    grunt.loadNpmTasks('grunt-filerev');
     grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-cache-breaker');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-connect');
 
-    grunt.registerTask('server', [
-        'clean', 'copy', 'ngdocs', 'ngmin', 'cssmin', 'cachebreaker', 'open:dev', 'open:docs', 'serve'
+    // Task for doing minify tasks on distribution version
+    grunt.registerTask('compress', [
+        'useminPrepare', 'concat', 'uglify:dev', 'cssmin:minify', 'usemin'
     ]);
 
+    // Task for watching JS errors during development (Errors detected will be outputed on console)
+    grunt.registerTask('jslinter', ['watch:js']);
+
+    // Developer related tasks
+    // ===============================================================================================
+
+    // $ grunt server (Launches development server)
+    grunt.registerTask('server', [
+        'wiredep', 'connect:dev', 'jslinter'
+    ]);
+
+    // $ grunt dist (Generates distribution version of application)
+    grunt.registerTask('release', [
+        'clean:all', 'wiredep', 'copy:main', 'compress', 'clean:temp', 'connect:dist'
+    ]);
+
+    // $ grunt docs (Launches doc portal)
+    grunt.registerTask('docs', [
+        'ngdocs', 'connect:docs'
+    ]);
 };
